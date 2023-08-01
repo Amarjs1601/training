74d54ec29e7f555b99f032b7d7a6b92f4ac9e986
Amarjs1601 <140712876+Amarjs1601@users.noreply.github.com>
2023-08-01 09:55:00 +0530

    vipul1

--- src/services/ServerServices.js
+++ src/services/ServerServices.js
@@ -1,5 +1,6 @@
 import axios from "axios"
 const ServerURL="http://192.168.1.62:5000" 
+
 // const ServerURL="http://10.0.2.2:5000"    
 
 //,{headers:{Authorization: "Bearer "+localStorage.getItem("TOKEN") }}
